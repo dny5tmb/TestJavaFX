c552f2d15e00b900b6205370e8c3ba103f0a4a64
dny5tmb <youhe@danny-go.com>
2014-11-30 21:21:46 +0900

    トグルボタンを使ってみて、Java側と対応させてみたりした。

--- src/main/java/com/dannygo/testjavafx/FXMLController.java
+++ src/main/java/com/dannygo/testjavafx/FXMLController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Toggle;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Effect;
 import javafx.scene.paint.Color;
@@ -17,10 +18,28 @@ public class FXMLController implements Initializable {
     private Label label;
     private int counter;
     @FXML
+    private Toggle tglAsa;
+    @FXML
+    private Toggle tglHiru;
+    @FXML
+    private Toggle tglYoru;
+    @FXML
+    private Toggle tglJapanese;
+    @FXML
+    private Toggle tglEnglish;
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         System.out.println("はろーわーるど。counter:" + counter++);
-        label.setText("Hello World!");
+        String msg = "";
+        if (tglAsa.isSelected()) {
+            msg = (tglJapanese.isSelected() ? "おはよう！" : "Good Morning!");
+        } else if (tglHiru.isSelected()) {
+            msg = (tglJapanese.isSelected() ? "こんにちわ！" : "Hello!");
+        } else if (tglYoru.isSelected()) {
+            msg = (tglJapanese.isSelected() ? "こんばんわ！" : "Good Night!");
+        }
+        label.setText(msg);
     }
 
     @FXML
